2aa52f27cc5ac717d6ca2e504129d4c0b4f2197e
MVPee <mariusvanpee@hotmail.be>
2024-09-07 13:01:40 +0200

    have vector of servers in main

--- main.cpp
+++ main.cpp
@@ -16,33 +16,45 @@ int main(int ac, char **av) {
     }
     if (file.is_open())
         file.close();
-    // while ((text.find("server") != std::string::npos) = position)
-    // {
-    //     // Continue le parsing à partir de position
-    // }
 
-    // std::cout << text << std::endl;
-    Server test(text);
-	std::cout << test << std::endl; //* DEBUG
-    try {
-        test.mySocket();
-        test.myBind();
-    }
-    catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        return (1);
+    std::vector<Server *> servers;
+    size_t start = 0;
+    int i = 0;
+    while ((start = text.find('{', start)) != std::string::npos) {
+        size_t end = text.find('}', start);
+        if (end == std::string::npos) {
+            std::cerr << "Not a valid conf file.." << std::endl;
+            return(1);
+        }
+        std::string config = text.substr(start + 1, end - start - 1);
+        Server *newServer = new Server(config);
+        servers.push_back(newServer);
+        start = end + 1;
+        i++;
     }
 
-    test.myListen();
-    while(1) {
+    for (size_t i = 0; i < servers.size(); i++) {
+        // std::cout << test << std::endl; //* DEBUG
         try {
-            test.process();
+            servers.at(i)->mySocket();
+            servers.at(i)->myBind();
         }
         catch (std::exception &e) {
             std::cerr << e.what() << std::endl;
             return (1);
         }
+        servers.at(i)->myListen();
     }
-
+    while(1) {
+        for (size_t i = 0; i < servers.size(); i++) {
+            try {
+                servers.at(i)->process();
+            }
+            catch (std::exception &e) {
+                std::cerr << e.what() << std::endl;
+                return (1);
+            }
+        }
+    }
     return (0);
 }
