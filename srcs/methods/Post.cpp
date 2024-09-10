9d028cc4b2cfb99c4d5e891c882db3b091e15084
Dan <spillebeendanilo@gmail.com>
2024-09-10 12:04:37 +0200

    Post use file structure for holding data

--- srcs/methods/Post.cpp
+++ srcs/methods/Post.cpp
@@ -1,16 +1,5 @@
 #include "../../includes/methods/Post.hpp"
 
-std::string get_data_in_header(std::string &header, std::string first_delimiter, std::string end_delimiter) {
-	std::size_t start = header.find(first_delimiter);
-	if (start == std::string::npos)
-		return (std::string());
-	std::string ret = header.substr(start + first_delimiter.size());
-	std::size_t end = ret.find(end_delimiter);
-	if (end == std::string::npos)
-		return (std::string());
-	return (ret.substr(0, end));
-}
-
 /*
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
@@ -23,13 +12,11 @@ _body_size(0),
 _status_code(request.get_status_code()),
 _state(ReceivingHeader) {
 	try {
-		std::string header = request.getHttpRequest();
+		const std::string header = request.getHttpRequest();
 		_boundary = get_data_in_header(header, "boundary=", "\r");
 		std::string content_length = get_data_in_header(header, "Content-Length: ", "\r");
 		std::string	content_type = get_data_in_header(header, "Content-Type: ", ";");
 
-		if (_boundary.empty() || content_length.empty() || content_type.empty())
-			throw_and_set_status(BAD_REQUEST, "boundary or content-length not found in header");
 
 		if (std::strtoul(content_length.c_str(), 0, 10) > server.getBody()) {
 			// fcntl(client_fd, F_SETFL, O_NONBLOCK);
@@ -38,7 +25,7 @@ _state(ReceivingHeader) {
 		}
 
 		if (content_type != "multipart/form-data")
-			throw_and_set_status(NOT_IMPLEMENTED, "Form is not a multipart/form-data");
+			throw_and_set_status(NOT_IMPLEMENTED, "Form is not a multipart/form-data"); //TODO gérer l'autre type de post
 
 	}
 	catch(const std::exception& e) {
@@ -74,77 +61,44 @@ std::ostream &			operator<<( std::ostream & o, Post const & i ) {
 
 void Post::decide_action ( std::string &new_content)
 {
-	if (_state == ReceivingHeader)
-	{
-		std::string total = _remaining_content + new_content;
+	try {
 		_body_size += new_content.size();
-		std::size_t pos;
-		if ((pos = total.find(HEADER_DELIMITER)) == std::string::npos)
-			_remaining_content = total;
-		else
+		if (_body_size > _server.getBody()) throw_and_set_status(PAYLOAD_TOO_LARGE, "Body size exceeded");
+
+		if (_state == ReceivingHeader)
 		{
-			pos+= HEADER_SIZE;
-			_state = HandlingBody;
-			_header = total.substr(0, pos);
-			_remaining_content = total.substr(pos, total.size());
-			new_content.clear();
+			std::string total = _remaining_content + new_content;
+			std::size_t pos;
+			if ((pos = total.find(HEADER_DELIMITER)) == std::string::npos)
+				_remaining_content = total;
+			else
+			{
+				pos+= HEADER_SIZE;
+				_state = HandlingBody;
+				_header = total.substr(0, pos);
+				_remaining_content = total.substr(pos, total.size());
+				new_content.clear();
+				get_file_infos();
+			}
+		}
+		if (_state == HandlingBody)
+		{
+			output_Content_Body(new_content);
+		}
+		if (_remaining_content == _boundary + "--\r\n")
+		{
+			_state = Completed;
+			clear_file_infos();
 		}
 	}
-	if (_state == HandlingBody)
-	{
-		_body_size += new_content.size();
-		handle_post_request(_request.getLocation(), new_content);
+	catch(const std::exception& e) {
+		if (_status_code == OK) _status_code = ERROR_INTERNAL_SERVER;
+		clear_file_infos();
+		std::cerr << R << e.what() << C << '\n';
 	}
-	if (_remaining_content == _boundary + "--\r\n")
-		_state = Completed;
-
 }
 
-void Post::handle_post_request(Location *location, std::string &new_content) {
-    std::string body_header (_header);
-
-
-	std::string contentDisposition = get_data_in_header(body_header, "Content-Disposition: ", ";");
-	if (contentDisposition.empty()) throw_and_set_status(BAD_REQUEST, "Content-disposition missing");
-
-	std::string filename = get_data_in_header(body_header, "filename=\"", "\"");
-	if (filename.empty()) throw_and_set_status(BAD_REQUEST, "filename missing");
-	filename = location->getRoot() + location->getUpload() + "/" + filename;
-	// std::cout << B << filename << C << std::endl; //* DEBUG
-
-    std::string contentType = get_data_in_header(body_header, "Content-Type: ", "\r");
-	if (contentType.empty()) throw_and_set_status(BAD_REQUEST, "Content-Type missing");
-
-	std::ofstream output_file(std::string(filename).c_str(), std::ofstream::app | std::ios::binary);
-	if (!output_file.is_open() || !output_file.good()) throw_and_set_status(ERROR_INTERNAL_SERVER, "Couldn't open file");
-
-
-	std::size_t pos = _remaining_content.find(_boundary);
-	if (pos != std::string::npos)
-	{
-		std::cout << G << "woui yé suis passé issi" C << std::endl;
-		output_file.write(_remaining_content.c_str(), pos);
-		_remaining_content.erase(0, pos);
-
-		if (!output_file.good())
-		{
-			remove(filename.c_str());
-			throw_and_set_status(ERROR_INTERNAL_SERVER, "Error with the output file");
-		}
-		_remaining_content += new_content;
-	}
-	else
-		output_Content_Body(output_file, filename, new_content);
-
-	//? DEBUG PRINTS
-    // std::cout << B << "Boundary: " << _boundary<< "\n";
-    // std::cout << "Content-Disposition: " << contentDisposition << "\n";
-    // std::cout << "Filename: " << filename << "\n";
-    // std::cout << "Content-Type: " << contentType << C << "\n" << std::endl;
-}
-
-
-void Post::output_Content_Body(std::ofstream &output_file, std::string &filename, std::string &new_content) {
+void Post::output_Content_Body(std::string &new_content) {
 	std::string	old_buffer = _remaining_content;
 
 
@@ -152,25 +106,30 @@ void Post::output_Content_Body(std::ofstream &output_file, std::string &filename
 	std::size_t end = full.find(_boundary);
 	if (end != std::string::npos)
 	{
-		output_file.write(full.c_str(), end);
+		_file.output_file.write(full.c_str(), end);
 		_remaining_content = full.substr(end, full.size() - end);
+		_state = ReceivingHeader;
 	}
 	else
 	{
-		output_file << _remaining_content;
+		_file.output_file << _remaining_content;
 		_remaining_content = new_content;
 	}
+	if (!_file.output_file.good()) throw_and_set_status(ERROR_INTERNAL_SERVER, "Error when writing in the output file");
 
+}
 
-	output_file.close();
-
-	if (!output_file.good() || _body_size > _server.getBody())
-		remove(filename.c_str());
-
-
-	if (_body_size > _server.getBody()) throw_and_set_status(PAYLOAD_TOO_LARGE, "Body size exceeded");
-	else if (!output_file.good()) throw_and_set_status(ERROR_INTERNAL_SERVER, "Error with the output file");
-
+std::string Post::get_data_in_header(const std::string &header, const std::string &first_delimiter, const std::string &end_delimiter) {
+	std::size_t start = header.find(first_delimiter);
+	if (start != std::string::npos)
+	{
+		std::string ret = header.substr(start + first_delimiter.size());
+		std::size_t end = ret.find(end_delimiter);
+		if (end != std::string::npos)
+			return (ret.substr(0, end));
+	}
+	throw_and_set_status(BAD_REQUEST, "'" + first_delimiter + "' missing");
+	return ("");
 }
 
 
@@ -179,6 +138,36 @@ void Post::throw_and_set_status(const size_t status_code, std::string message)
 	_status_code = status_code;
 	throw std::runtime_error(message);
 }
+
+
+void	Post::get_file_infos( void )
+{
+	clear_file_infos();
+	_file.contentDisposition = get_data_in_header(_header, "Content-Disposition: ", ";");
+
+	_file.filename = get_data_in_header(_header, "filename=\"", "\"");
+	_file.filename = _request.getLocation()->getRoot() + _request.getLocation()->getUpload() + "/" + _file.filename;
+
+    _file.contentType = get_data_in_header(_header, "Content-Type: ", "\r");
+
+	_file.output_file.open(_file.filename.c_str(), std::ofstream::app | std::ios::binary);
+	if (!_file.output_file.is_open() || !_file.output_file.good()) throw_and_set_status(ERROR_INTERNAL_SERVER, "Couldn't open file");
+	
+}
+void	Post::clear_file_infos ( void )
+{
+	_file.contentDisposition.clear();
+	_file.filename.clear();
+    _file.contentType.clear();
+	if (_file.output_file.is_open())
+	{
+		if (!_file.output_file.good() || (_status_code != OK && _status_code != NO_CONTENT))
+			remove(_file.filename.c_str());
+		_file.output_file.close();
+	}
+	_file.output_file.clear();
+}
+//TODO Check que c'est bien un fichier ?
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
