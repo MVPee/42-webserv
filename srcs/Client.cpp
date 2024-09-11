ac4c5fdb4ce549c3eed995ee2bb5ee26ea68d617
Marius Van Pee <mariusvanpee@hotmail.be>
2024-09-11 14:13:38 +0200

    little fix

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -88,22 +88,21 @@ void Client::request( void )
 }
 
 void Client::clear(void) {
-	if (_client_fd) {
+	if (_client_fd)
 		close(_client_fd);
-		_client_fd = 0;
-		_header.clear();
-		_body.clear();
-		_response.clear();
-		_state = ReceivingHeader;
-		_connection_time = -1;
-		if (_request) {
-			delete _request;
-			_request = 0;
-		}
-		if (_post) {
-			delete _post;
-			_post = 0;
-		}
+	_client_fd = 0;
+	_header.clear();
+	_body.clear();
+	_response.clear();
+	_state = ReceivingHeader;
+	_connection_time = -1;
+	if (_request) {
+		delete _request;
+		_request = 0;
+	}
+	if (_post) {
+		delete _post;
+		_post = 0;
 	}
 }
 
